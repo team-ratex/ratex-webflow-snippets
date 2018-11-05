3281d767cdf602f74a0672b0ae1988cb42ec9c25
JingRong ( <jing_rong92@hotmail.com>
2018-11-05 17:10:40 +0800

    stable-ish api

--- shopathon-deals/singlesdaysales.js
+++ shopathon-deals/singlesdaysales.js
@@ -1,26 +1,42 @@
 class Shopathon {
   constructor(numberOfDeals) {
+    this.numberOfDeals = numberOfDeals;
     this.url = `https://ratex.co/store/api/products?filter=LATEST&limit=${numberOfDeals}`;
     this.page = 1;
     this.dealsParentContainer = '#deals-parent';
-    this.tabContentContainer = '.w-tab-content';
+    this.tabContentContainer = '.w-tab-content'; // TODO: This needs to be more specific
+    this.backButtonId = 'deals-custom-back';
+    this.nextButtonId = 'deals-custom-next';
+    this.hasMore = null;
   }
   // Basically our driver function
   setUpPage() {
-    this.renderPagination();
+    this.renderPagination(true, false); // Back always disabled on first page
+    this.clearOutAllExistingDeals();
     this.populateDeals();
+    this.setUpPaginationButtonListeners();
   }
-  renderPagination() {
+  renderPagination(backDisabled, nextDisabled) {
     // Create Back and Next Buttons
+    // Back
     const BackButton = document.createElement("div");
     BackButton.classList.add("w-tab-link");
     BackButton.style.margin = '20px 36px';
+    BackButton.style.opacity = (backDisabled) ? 0.6 : 1;
+    BackButton.style.cursor = (backDisabled)
+      ? 'not-allowed' : 'cursor';
+    BackButton.id = this.backButtonId;
     BackButton.innerText = "Back"
+    // Next
     const NextButton = document.createElement("div");
     NextButton.style.margin = '20px 36px';
     NextButton.classList.add("w-tab-link");
-
+    NextButton.style.opacity = (nextDisabled) ? 0.6 : 1;
+    NextButton.style.cursor = (nextDisabled)
+      ? 'not-allowed' : 'cursor';
+    NextButton.id = this.nextButtonId;
     NextButton.innerText = "Next"
+
     // Set up pagination Element
     const PaginationElement = document.createElement("div");
     PaginationElement.style.display = 'flex';
@@ -46,8 +62,50 @@ class Shopathon {
           );
           $(this.dealsParentContainer).append(deal.getDomElement())
         });
+        this.hasMore = response.hasMore;
+        this.toggleNextButtonAvailability(response.hasMore);
       }
     });
+  }
+  setUpPaginationButtonListeners() {
+    $(`#${this.backButtonId}`).click(() => {
+      if (this.page > 1) {
+        // Update page number
+        this.page -= 1;
+        // Update url
+        this.url = `https://ratex.co/store/api/products?filter=LATEST&limit=${this.numberOfDeals}&offset=${this.numberOfDeals * (this.page - 1)}`;
+        // Clear out old doms
+        this.clearOutAllExistingDeals();
+        // re-populate deals
+        this.populateDeals();
+      }
+    })
+    $(`#${this.nextButtonId}`).click(() => {
+      // Update page number
+      this.page += 1;
+      // Update url
+      this.url = `https://ratex.co/store/api/products?filter=LATEST&limit=${this.numberOfDeals}&offset=${this.numberOfDeals * (this.page - 1)}`;
+      // Clear out old doms
+      this.clearOutAllExistingDeals();
+      // re-populate deals
+      this.populateDeals();
+      this.toggleBackButtonAvailability(true);
+    })
+  }
+  clearOutAllExistingDeals() {
+    $(this.dealsParentContainer)[0].innerHTML = "";
+  }
+  toggleBackButtonAvailability(enabled) {
+    const BackButton = document.getElementById(this.backButtonId);
+    BackButton.style.opacity = (enabled) ? 1 : 0.6;
+    BackButton.style.cursor = (enabled)
+      ? 'cursor' : 'not-allowed';
+  }
+  toggleNextButtonAvailability(enabled) {
+    const NextButton = document.getElementById(this.nextButtonId);
+    NextButton.style.opacity = (enabled) ? 1 : 0.6;
+    NextButton.style.cursor = (enabled)
+      ? 'cursor' : 'not-allowed';
   }
 }
 class DealCell {
@@ -78,7 +136,10 @@ class DealCell {
         </a>
         <div class="products2-description-wrap">
           <h3 class="heading-50">${this.merchant}</h3>
-          <p class="text-14 text-14-60">${this.name}</p>
+          <p
+            class="text-14 text-14-60"
+            style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"
+          >${this.name}</p>
         </div>
       </div>
     `;
