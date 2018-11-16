a43c80a45fcfc69ed70fd50dbf4e675501455a9c
JingRong ( <jing_rong92@hotmail.com>
2018-11-16 14:54:55 +0800

    update styling

--- shopathon-deals/blackfridaysales.js
+++ shopathon-deals/blackfridaysales.js
@@ -259,7 +259,7 @@ class DealCell {
           <div class="div-block-252">
             <div
               class="text-block-189 _1"
-              style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; height: 2.2em;"
+              style="overflow: hidden; text-overflow: ellipsis; display: -webkit-box; -webkit-line-clamp: 2; -webkit-box-orient: vertical; height: 2.6667em;"
               >
               ${this.name}
             </div>
