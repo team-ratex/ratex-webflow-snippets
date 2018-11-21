bd05310a2c6f0aa739015e780ed170b75f095596
JingRong ( <jing_rong92@hotmail.com>
2018-11-21 19:25:04 +0800

    all to go to appstore since deep link is not up

--- rates-download-modifier.js
+++ rates-download-modifier.js
@@ -16,6 +16,13 @@ $(() => {
   $('.install-app-button').each((idx) => {
     const element = $('.install-app-button')[idx];
     element.target = '_blank';
-    element.href = 'ratesbyrate://';
+    //iPhone Version:
+    if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+      element.href = 'https://itunes.apple.com/sg/app/rates-mobile-app-by-ratex/id1350096340?mt=8'
+    }
+    //Android Version:
+    if (navigator.userAgent.match(/android/i)) {
+      element.href = 'https://play.google.com/store/apps/details?id=com.rate.rates'
+    }
   });
 })
