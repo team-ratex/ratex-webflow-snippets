ce1265b774f955f01d3e1a328ab68aea9e4766e7
JingRong ( <jing_rong92@hotmail.com>
2018-11-21 19:30:35 +0800

    Test with deeplink

--- rates-download-modifier.js
+++ rates-download-modifier.js
@@ -17,12 +17,12 @@ $(() => {
     const element = $('.install-app-button')[idx];
     element.target = '_blank';
     //iPhone Version:
-    if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
-      element.href = 'https://itunes.apple.com/sg/app/rates-mobile-app-by-ratex/id1350096340?mt=8'
-    }
-    //Android Version:
-    if (navigator.userAgent.match(/android/i)) {
-      element.href = 'https://play.google.com/store/apps/details?id=com.rate.rates'
-    }
+    element.href = 'ratesbyrate://';
+    // if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+    // }
+    // //Android Version:
+    // if (navigator.userAgent.match(/android/i)) {
+    //   element.href = 'https://play.google.com/store/apps/details?id=com.rate.rates'
+    // }
   });
 })
