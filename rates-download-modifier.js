5189406ade34e27ad2ec50d639883d496219f75d
JingRong ( <jing_rong92@hotmail.com>
2018-11-21 19:22:40 +0800

    init

---
+++ rates-download-modifier.js
@@ -0,0 +1,21 @@
+$(() => {
+  // Install button modifier
+  $('.install-app-button').each((idx) => {
+    const element = $('.install-app-button')[idx];
+    element.target = '_blank';
+    //iPhone Version:
+    if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+      element.href = 'https://itunes.apple.com/sg/app/rates-mobile-app-by-ratex/id1350096340?mt=8'
+    }
+    //Android Version:
+    if (navigator.userAgent.match(/android/i)) {
+      element.href = 'https://play.google.com/store/apps/details?id=com.rate.rates'
+    }
+  });
+  // Open in rates modifier
+  $('.install-app-button').each((idx) => {
+    const element = $('.install-app-button')[idx];
+    element.target = '_blank';
+    element.href = 'ratesbyrate://';
+  });
+})
