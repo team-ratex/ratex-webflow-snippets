4f7f70a81785a2612e467f9fef37f3d44bf3d066
Programmingisfun93 <ChernHwee.Low_2015@SIT.SingaporeTech.edu.sg>
2019-04-15 18:49:04 +0800

    add closure such that other functions are unable to be called

--- ratex-popup/popup.js
+++ ratex-popup/popup.js
@@ -1,81 +1,83 @@
-let popupData = {};
-let repeatIdCheck = [];
-let initalTimeup;
-let followUpTimeup;
-let durationTimeUp;
-let appearDuration;
+// closure used lexical scoping such that other functions are not be able to called
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+function popupInit(initalTime = 10000, followUpTime = 10000, duration = 5000) {
+  let popupData = {};
+  let repeatIdCheck = [];
+  let initalTimeup;
+  let followUpTimeup;
+  let durationTimeUp;
+  let appearDuration;
 
-// enum 
-const localStorageKey = 'popupIdList';
-const popup = {
-  popupContainerClass: 'popupContainer',
-  imageId: 'imageContent',
-  titleId: 'title',
-  nameId: 'name',
-  timeId: 'time',
-  closeId: 'close'
-}
-
-// Set up the app fomo popup
-function popupSetup(initalTime = 10000, followUpTime = 10000, duration = 5000) {
-  const hasPopupIdList = localStorage.getItem(localStorageKey) !== null &&
-    Array.isArray(JSON.parse(localStorage.getItem(localStorageKey)));
-  if (hasPopupIdList) {
-    repeatIdCheck = JSON.parse(localStorage.getItem(localStorageKey));
+  // enum 
+  const localStorageKey = 'popupIdList';
+  const popup = {
+    popupContainerClass: 'popupContainer',
+    imageId: 'imageContent',
+    titleId: 'title',
+    nameId: 'name',
+    timeId: 'time',
+    closeId: 'close'
   }
-  const popupContainer = document.getElementsByClassName(popup.popupContainerClass)[0];
-  const retreivalTimelimit = 10000;
-  const durationLimit = 5000;
-  // Prevent calling popupSetup function twice
-  if (!popupContainer) {
-    if (typeof initalTime !== 'number' || initalTime <0 ){
+
+  // Set up the app fomo popup
+  function setup(){
+    const hasPopupIdList = localStorage.getItem(localStorageKey) !== null &&
+      Array.isArray(JSON.parse(localStorage.getItem(localStorageKey)));
+    if (hasPopupIdList) {
+      repeatIdCheck = JSON.parse(localStorage.getItem(localStorageKey));
+    }
+    const popupContainer = document.getElementsByClassName(popup.popupContainerClass)[0];
+    const retreivalTimelimit = 10000;
+    const durationLimit = 5000;
+    // Prevent calling popupSetup function twice
+    if (!popupContainer) {
+      if (typeof initalTime !== 'number' || initalTime < 0) {
         initalTime = retreivalTimelimit;
-    }
-    if (typeof followUpTime !== 'number' || followUpTime < retreivalTimelimit) {
-      followUpTime = retreivalTimelimit
-    }
-    if (typeof duration !== 'number' || duration < 0 || duration >= followUpTime) {
-      //1s faded in , 1s faded out
-      duration = followUpTime - 2000;
-    }
-    if (duration < durationLimit) {
-      duration = durationLimit;
-    }
-    appearDuration = duration;
-    createDisplay();
-    // when time reaches the initalTime , intervally calls the incoming data, retrieve data and display. 
-    initalTimeup = setTimeout(function () {
-      incomingIntervalUpdate(followUpTime);
-      retrieveData();
-    }, initalTime);
-  }
-}
-
-// Create the display of app fomo
-function createDisplay() {
-  const popupElement = document.createElement('div');
-  popupElement.className = popup.popupContainerClass;
-  popupElement.id = 'popupContainerOut';
-  popupElement.innerHTML =
-    "<img id=" + popup.imageId +" />" +
-    "<div class=textContainer>" +
-    "<div class=closeContainer>" +
-    "<div id=" + popup.titleId + "></div>" + 
-    "<img src='https://raw.githubusercontent.com/rate-engineering/ratex-webflow-snippets/44ee8793cc84d126b9ecf0f36bdc9f4dd1c2bfbf/ClosePopup.svg' alt='close' id=" + popup.closeId+" />" +
-    "</div>" +
-    "<div id=" + popup.nameId +"></div>" +
-    "<div id=" + popup.timeId +"></div>";
-  document.body.appendChild(popupElement);
-  popupElement.onclick = function (e) {
-    if (e.target.id === popup.closeId) {
-      closePopup();
-    } else {
-      openInNewTab();
+      }
+      if (typeof followUpTime !== 'number' || followUpTime < retreivalTimelimit) {
+        followUpTime = retreivalTimelimit
+      }
+      if (typeof duration !== 'number' || duration < 0 || duration >= followUpTime) {
+        //1s faded in , 1s faded out
+        duration = followUpTime - 2000;
+      }
+      if (duration < durationLimit) {
+        duration = durationLimit;
+      }
+      appearDuration = duration;
+      createDisplay();
+      // when time reaches the initalTime , intervally calls the incoming data, retrieve data and display. 
+      initalTimeup = setTimeout(function () {
+        incomingIntervalUpdate(followUpTime);
+        retrieveData();
+      }, initalTime);
     }
   }
-}
-// Retrieve the data from ratex.co/store/api/feed
-function retrieveData() {
+  // Create the display of app fomo
+  function createDisplay() {
+    const popupElement = document.createElement('div');
+    popupElement.className = popup.popupContainerClass;
+    popupElement.id = 'popupContainerOut';
+    popupElement.innerHTML =
+      "<img id=" + popup.imageId + " />" +
+      "<div class=textContainer>" +
+      "<div class=closeContainer>" +
+      "<div id=" + popup.titleId + "></div>" +
+      "<img src='https://raw.githubusercontent.com/rate-engineering/ratex-webflow-snippets/44ee8793cc84d126b9ecf0f36bdc9f4dd1c2bfbf/ClosePopup.svg' alt='close' id=" + popup.closeId + " />" +
+      "</div>" +
+      "<div id=" + popup.nameId + "></div>" +
+      "<div id=" + popup.timeId + "></div>";
+    document.body.appendChild(popupElement);
+    popupElement.onclick = function (e) {
+      if (e.target.id === popup.closeId) {
+        closePopup();
+      } else {
+        openInNewTab();
+      }
+    }
+  }
+  // Retrieve the data from ratex.co/store/api/feed
+  function retrieveData() {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://ratex.co/store/api/feed', true);
     xhr.addEventListener("load", function () {
@@ -86,7 +88,7 @@ function retrieveData() {
           popupData.title = "Someone recently purchased";
           popupData.name = data.data[0].product.name;
           popupData.image = data.data[0].product.images[0];
-          popupData.id = "P"+ data.data[0].product.id.toString();
+          popupData.id = "P" + data.data[0].product.id.toString();
           popupData.link = "https://ratex.co/home/" + data.data[0].product.slug + "/p/" + data.data[0].product.id;
         } else {
           popupData.title = "Someone recently used";
@@ -97,96 +99,99 @@ function retrieveData() {
         }
         updateDisplay();
       }
-    }, false );
+    }, false);
     xhr.send();
-}
+  }
 
-// Calcute the difference between today's time and data's time 
-function calTimeDiff(time) {
-  const todayDate = new Date();
-  const compareDate = new Date(time);
-  const timeDiff = Math.abs(todayDate.getTime() - compareDate.getTime());
-  const minutes = 60 * 1000;
-  const hours = minutes * 60;
-  const day = hours * 24;
-  let diffDays;
-  if (timeDiff > minutes) {
-    diffDays = Math.ceil(timeDiff / minutes).toString() + " minutes ago";
+  // Calcute the difference between today's time and data's time 
+  function calTimeDiff(time) {
+    const todayDate = new Date();
+    const compareDate = new Date(time);
+    const timeDiff = Math.abs(todayDate.getTime() - compareDate.getTime());
+    const minutes = 60 * 1000;
+    const hours = minutes * 60;
+    const day = hours * 24;
+    let diffDays;
+    if (timeDiff > minutes) {
+      diffDays = Math.ceil(timeDiff / minutes).toString() + " minutes ago";
+    }
+    if (timeDiff > hours) {
+      diffDays = Math.ceil(timeDiff / hours).toString() + " hours ago";
+    }
+    if (timeDiff > day) {
+      diffDays = Math.ceil(timeDiff / day).toString() + " days ago";
+    }
+    return diffDays;
   }
-  if (timeDiff > hours) {
-    diffDays = Math.ceil(timeDiff / hours).toString() + " hours ago";
-  }
-  if (timeDiff > day) {
-    diffDays = Math.ceil(timeDiff / day).toString() + " days ago";
-  }
-  return diffDays;
-}
 
-// Update the display
-function updateDisplay() {
-  if (repeatIdCheck.indexOf(popupData.id) === -1) {
-    const imageContent = document.getElementById(popup.imageId);
-    if (imageContent) {
-      imageContent.setAttribute('alt', popupData.name);
-      imageContent.setAttribute('src', popupData.image);
+  // Update the display
+  function updateDisplay() {
+    if (repeatIdCheck.indexOf(popupData.id) === -1) {
+      const imageContent = document.getElementById(popup.imageId);
+      if (imageContent) {
+        imageContent.setAttribute('alt', popupData.name);
+        imageContent.setAttribute('src', popupData.image);
+      }
+      const title = document.getElementById(popup.titleId);
+      if (title) {
+        title.innerText = popupData.title;
+      }
+      const name = document.getElementById(popup.nameId);
+      if (name) {
+        name.innerText = popupData.name;
+      }
+      const time = document.getElementById(popup.timeId);
+      if (time) {
+        time.innerText = popupData.date;
+      }
+      const popupContainer = document.getElementsByClassName(popup.popupContainerClass)[0];
+      if (popupContainer) {
+        popupContainer.id = "popupContainerEntrance";
+      }
+      // update the check
+      repeatIdCheck.push(popupData.id);
+      localStorage.setItem(localStorageKey, JSON.stringify(repeatIdCheck));
+      appearDurationBeforeExit();
     }
-    const title = document.getElementById(popup.titleId);
-    if (title) {
-      title.innerText = popupData.title;
+  }
+
+  // Duration that the app fomo appears before it disappear
+  function appearDurationBeforeExit() {
+    durationTimeUp = setTimeout(function () {
+      const popupContainer = document.getElementsByClassName(popup.popupContainerClass)[0];
+      if (popupContainer) {
+        popupContainer.id = "popupContainerExit";
+      }
+    }, appearDuration);
+  }
+
+  // Handles at an interval of followUp time to get the data 
+  function incomingIntervalUpdate(followUpTime) {
+    followUpTimeup = setInterval(function () {
+      retrieveData();
+    }, followUpTime);
+  }
+
+  // Open a new tab base on the link
+  function openInNewTab() {
+    if (!!popupData.link) {
+      const win = window.open(popupData.link, '_blank');
+      win.focus();
     }
-    const name = document.getElementById(popup.nameId);
-    if (name) {
-      name.innerText = popupData.name;
-    }
-    const time = document.getElementById(popup.timeId);
-    if (time) {
-      time.innerText = popupData.date;
-    }
+  }
+
+  // Close the app fomo popup
+  function closePopup() {
     const popupContainer = document.getElementsByClassName(popup.popupContainerClass)[0];
     if (popupContainer) {
-      popupContainer.id = "popupContainerEntrance";
+      // To make it "disappear"
+      popup.id = "popupContainerExit";
     }
-    // update the check
-    repeatIdCheck.push(popupData.id);
-    localStorage.setItem(localStorageKey, JSON.stringify(repeatIdCheck));
-    appearDurationBeforeExit();
+    // Stop the time
+    clearTimeout(initalTimeup);
+    clearInterval(followUpTimeup);
+    clearTimeout(durationTimeUp);
   }
-}
 
-// Duration that the app fomo appears before it disappear
-function appearDurationBeforeExit() {
-  durationTimeUp = setTimeout(function () {
-    const popupContainer = document.getElementsByClassName(popup.popupContainerClass)[0];
-    if (popupContainer) {
-      popupContainer.id = "popupContainerExit";
-    }
-  }, appearDuration);
-}
-
-// Handles at an interval of followUp time to get the data 
-function incomingIntervalUpdate(followUpTime) {
-  followUpTimeup = setInterval(function () {
-    retrieveData();
-  }, followUpTime);
-}
-
-// Open a new tab base on the link
-function openInNewTab() {
-  if(!!popupData.link) {
-    const win = window.open(popupData.link, '_blank');
-    win.focus();
-  }
-}
-
-// Close the app fomo popup
-function closePopup() {
-  const popupContainer = document.getElementsByClassName(popup.popupContainerClass)[0];
-  if (popupContainer) {
-    // To make it "disappear"
-    popupContainer.id = "popupContainerExit";
-  }
-  // Stop the time
-  clearTimeout(initalTimeup);
-  clearInterval(followUpTimeup);
-  clearTimeout(durationTimeUp);
+  setup();
 }
